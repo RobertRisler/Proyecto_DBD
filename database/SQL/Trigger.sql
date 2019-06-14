06a4f4574580de96346d51d0607d6255506b0037
RobertRisler <roberto.lillo@usach.cl>
2019-06-14 15:41:55 -0400

    Agregado trigger mesa a SQL

--- database/SQL/Trigger.sql
+++ database/SQL/Trigger.sql
@@ -42,3 +42,25 @@ CREATE TRIGGER asignar_rol
 AFTER INSERT ON usuarios
 FOR EACH ROW
 EXECUTE PROCEDURE dar_rol();
+
+/* Asigna la cantidad generica de sillas a una mesa con 0 sillas */
+CREATE OR REPLACE FUNCTION mesas_por_defecto()
+RETURNS trigger AS
+$$
+    BEGIN
+    IF NEW.cantidad_asientos = 0 THEN
+        UPDATE mesas
+        SET cantidad_asientos = 4
+        WHERE mesas.id = NEW.id;
+    END IF;
+    RETURN NEW;
+    END;
+$$
+LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS cantidad_sillas ON mesas;
+
+CREATE TRIGGER cantidad_sillas
+AFTER INSERT ON mesas
+FOR EACH ROW
+EXECUTE PROCEDURE mesas_por_defecto();
