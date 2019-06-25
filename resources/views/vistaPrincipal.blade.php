a020923031db38b1cab4770a6a560c21384e5083
hugo-arenas <hugo.arenas@usach.cl>
2019-06-24 22:34:36 -0400

    borrador de vistaPrincipal

---
+++ resources/views/vistaPrincipal.blade.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+    <title>Untitled</title>
+    <link rel="stylesheet" href="bootstrap.min.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Abril+Fatface" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Acme" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Amita" />
+    <link rel="stylesheet" href="styles.css" />
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-md" style="filter: brightness(101%);">
+        <div class="container-fluid"><a href="#" class="navbar-brand" style="color: rgba(255,255,255,0.9);">TuComida</a><button data-toggle="collapse" data-target="#navcol-1" class="navbar-toggler"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div
+                class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li role="presentation" class="nav-item"><a href="#" class="nav-link active" style="color: #ffffff;">First Item</a></li>
+                    <li role="presentation" class="nav-item"><a href="#" class="nav-link" style="color: #ffffff;">Crear cuenta</a></li>
+                    <li role="presentation" class="nav-item"><a href="#" class="nav-link" style="color: #ffffff;">Ingresar</a></li>
+                </ul>
+        </div>
+        </div>
+    </nav>
+    <div class="jumbotron" style="background-color: rgb(137,255,205);">
+        <h1 class="text-center" style="font-family: Amita, cursive;color: rgb(0,0,0);"><strong>TuComida.com</strong></h1>
+        <p class="text-center" style="font-size: 20px;font-family: Acme, sans-serif;color: rgb(0,0,0);"><strong>Reserva tu mesa y pide tu comida</strong></p>
+        <p><a class="btn btn-primary" role="button" href="#">Learn more</a></p>
+    </div>
+    <div style="width: auto;">
+        <ul class="text-center d-lg-flex restaurantesRec" style="font-size: 19px;margin: 0px;padding: 0px;background-color: #ffffff;height: auto;width: 445px;">
+            <li class="border rounded-0 producto">
+                <div class="text-left"><img style="background-image: url('domino.jpg');width: 215px;height: 215px;" /></div>
+                <div style="width: auto;"><span>Dominó Maipú Centro </span><span>Av. Los Pajaritos</span></div>
+            </li>
+            <li class="border rounded-0 producto">
+                <div class="text-left"><img style="background-image: url('pizzahut.png');width: 215px;height: 215px;" /></div>
+                <div style="width: auto;"><span>Pizza Hut Maipú Centro </span><span>Av. Los Pajaeritos</span></div>
+            </li>
+        </ul>
+    </div>
+</body>
+
+</html>
